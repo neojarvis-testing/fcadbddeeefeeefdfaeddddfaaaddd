7c1c32ba233f84aa0f54bbe7d056b188b56eeb46
neojarvis <techadmin@iamneo.ai>
2025-05-27 07:02:09 +0000

    initialCommit-Boilerplate

---
+++ springappmart/src/main/java/com/examly/springappmart/SpringappmartApplication.java
@@ -0,0 +1,13 @@
+package com.examly.springappmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringappmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringappmartApplication.class, args);
+	}
+
+}
